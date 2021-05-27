5bcf711b1977fbd8e40d3c96cad2aa89bdbd12a9
Oliver Layer <olilay@live.de>
2021-05-27 20:34:51 +0200

    basic client implementation

--- src/client.rs
+++ src/client.rs
@@ -1,3 +1,5 @@
+use std::{io::Error, net::Ipv4Addr};
+
 use crate::{arp::ArpMessage, interfaces::Interface};
 use pnet::{
     datalink::{channel, Channel, DataLinkReceiver},
@@ -5,6 +7,7 @@ use pnet::{
         arp::ArpPacket,
         ethernet::{EtherTypes, EthernetPacket, MutableEthernetPacket},
     },
+    util::MacAddr,
 };
 
 pub struct ArpClient {
@@ -13,25 +16,57 @@ pub struct ArpClient {
 }
 
 impl ArpClient {
-    pub fn new(interface: &Interface) -> Self {
+    pub fn new() -> Self {
+        ArpClient::new_with_iface(&Interface::new())
+    }
+
+    pub fn new_with_iface(interface: &Interface) -> Self {
         let rx = match channel(&interface.get_raw_interface(), Default::default()) {
             Ok(Channel::Ethernet(_, rx)) => rx,
             Ok(_) => panic!("Unknown channel type"),
             Err(err) => panic!("Error when opening channel: {}", err),
         };
 
-        ArpClient { rx_channel: rx, interface: interface.clone() }
+        ArpClient {
+            rx_channel: rx,
+            interface: interface.clone(),
+        }
     }
 
-    fn next_ethernet_frame(&mut self) -> Option<(EthernetPacket, &[u8])> {
-        let rx_bytes = match self.rx_channel.next() {
-            Ok(rx_bytes) => rx_bytes,
-            Err(_) => return None,
-        };
+    pub fn ip_to_mac(&mut self, ip_addr: Ipv4Addr) -> Result<MacAddr, Error> {
+        let message =
+            ArpMessage::new_arp_request(self.interface.get_mac(), self.interface.get_ip(), ip_addr);
 
-        match EthernetPacket::new(&rx_bytes) {
-            Some(frame) => Some((frame, rx_bytes)),
-            None => None,
+        match message.send(&self.interface) {
+            Err(e) => return Err(e),
+            _ => {}
+        }
+
+        loop {
+            match self.next() {
+                Some(arp_message) if arp_message.source_protocol_address == ip_addr => {
+                    return Ok(arp_message.source_hardware_address)
+                }
+                _ => {}
+            }
+        }
+    }
+
+    pub fn mac_to_ip(&mut self, mac_addr: MacAddr) -> Result<Ipv4Addr, Error> {
+        let message = ArpMessage::new_rarp_request(self.interface.get_mac(), mac_addr);
+
+        match message.send(&self.interface) {
+            Err(e) => return Err(e),
+            _ => {}
+        }
+
+        loop {
+            match self.next() {
+                Some(arp_message) if arp_message.source_hardware_address == mac_addr => {
+                    return Ok(arp_message.target_protocol_address)
+                }
+                _ => {}
+            }
         }
     }
 
@@ -52,6 +87,18 @@ impl ArpClient {
                 }
                 _ => return None,
             }
+        }
+    }
+
+    fn next_ethernet_frame(&mut self) -> Option<(EthernetPacket, &[u8])> {
+        let rx_bytes = match self.rx_channel.next() {
+            Ok(rx_bytes) => rx_bytes,
+            Err(_) => return None,
+        };
+
+        match EthernetPacket::new(&rx_bytes) {
+            Some(frame) => Some((frame, rx_bytes)),
+            None => None,
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,61 +2,20 @@ mod arp;
 mod client;
 mod interfaces;
 
-use arp::ArpMessage;
-use interfaces::Interface;
+use std::net::Ipv4Addr;
+
 use client::ArpClient;
-use pnet::packet::arp::ArpHardwareTypes;
-use pnet::packet::arp::{ArpPacket, MutableArpPacket};
-use pnet::packet::ethernet::{EtherTypes, EthernetPacket, MutableEthernetPacket};
-use pnet::{
-    datalink::{channel, Channel, MacAddr},
-    packet::{arp::ArpOperation, MutablePacket, Packet},
-};
-use std::net::{IpAddr, Ipv4Addr};
+use pnet::util::MacAddr;
 
 fn main() {
-    let interface = Interface::new();
-
     let searched_mac = MacAddr::new(0xdc, 0xa6, 0x32, 0x27, 0x5b, 0xd8);
 
-    let arp_message = ArpMessage::new_rarp_request(
-        interface.get_mac(),
-        searched_mac,
-    );
 
-    arp_message.send(&interface).unwrap();
+    let mut client = ArpClient::new();
 
-    let mut client = ArpClient::new(&interface);
+    let result = client.mac_to_ip(searched_mac);
+    println!("le answer #1 is {}", result.unwrap());
 
-    loop {
-        match client.next() {
-            Some(arp_message) if arp_message.source_hardware_address == searched_mac => {
-                println!("le answer is {}", arp_message.source_protocol_address);
-                return;
-            }
-            Some(_) => {
-                println!("Received ARP message, but not an answer to before.")
-            }
-            None => {}
-        }
-    }
-
-    /*
-    while true {
-        let rx_buf = rx.next().unwrap();
-        let received_eth = EthernetPacket::new(&rx_buf);
-
-        match received_eth {
-            Some(packet) if packet.get_ethertype() == EtherTypes::Rarp => {
-                let arp = ArpPacket::new(&rx_buf[MutableEthernetPacket::minimum_packet_size()..])
-                    .unwrap();
-
-                println!("Received reply {}", arp.get_sender_proto_addr());
-                return;
-            }
-            Some(_) => {}
-            None => {}
-        }
-    }
-    */
+    let result = client.ip_to_mac(Ipv4Addr::new(10, 0, 0, 2));
+    println!("le answer #2 is {}", result.unwrap());
 }
